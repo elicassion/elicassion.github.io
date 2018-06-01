a56f3f68b6ba1f37a6f897b93ac1f766d41dd49f
etoss <elicassion@sjtu.edu.cn>
2018-06-01 10:26:55 +0800

    add missing jquery.waypoints

--- js/timeline.js
+++ js/timeline.js
@@ -13,6 +13,6 @@ function customWayPoint(className, addClassName, customOffset) {
 
 var defaultOffset = '50%';
 
-for (i = 0; i < 17; i++) {
+for (var i = 0; i < 17; i++) {
     customWayPoint('.timeline__item--' + i, 'timeline__item-bg', defaultOffset);
 }
